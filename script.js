8a3cb94b38cde735e6c4f1131f3b03abfd0c02ee
Greg Howes <gihowes@gmail.com>
2023-08-21 14:03:33 +0900

    Gameplay functionality added That was a good coding session Can now add tokens to the correct square and logic will check for a winner and report to the players

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const Player = (name, symbol) => {
 
 const test = function(square) {
     console.log(square);
+    square.srcElement.innerHTML = "X";
 }
 
 //Module to store and manipulate game board internally
@@ -21,8 +22,9 @@ const gameBoard = (() => {
 
     const initialiseGameBoard = () => {    
         gameBoardArray = [];
-        for (i=0; i<=9; i++) {        
-            gameBoardArray.push(i);
+        for (i=0; i<=9; i++) {     
+            //add a space to each square   
+            gameBoardArray.push(" ");
         }
     };
 
@@ -30,6 +32,77 @@ const gameBoard = (() => {
     const newGame = () => {
         initialiseGameBoard();
         displayController.renderBoard(getBoardStatus());
+        currentPlayer = "O";
+
+    }
+
+    //internal function to switch player
+    const switchPlayer = () => {
+        if (currentPlayer == "O") {
+            currentPlayer = "X";
+        } else {
+            currentPlayer = "O";
+        }
+    }
+
+    //internal function to check for a winner
+    //I know there must be a less 'muscular' way to do things than this,
+    //but this will work for now!
+    const checkforWinner = (player) => {
+        /*
+                0 1 2
+                3 4 5
+                6 7 8
+            */
+        //check for diagonal wins
+        if( (gameBoardArray[0] == player && gameBoardArray[4] == player && gameBoardArray[8] == player) ||
+            (gameBoardArray[2] == player && gameBoardArray[4] == player && gameBoardArray[6] == player)) {
+            console.log("Winner");
+            return true;
+        } else if 
+            //check for horizontal wins
+            ((gameBoardArray[0] == player && gameBoardArray[1] == player && gameBoardArray[2] == player) ||
+             (gameBoardArray[3] == player && gameBoardArray[4] == player && gameBoardArray[5] == player) || 
+             (gameBoardArray[6] == player && gameBoardArray[7] == player && gameBoardArray[8] == player)) {
+                console.log("Winner");
+                return true;   
+        } else if 
+            //check for vertical wins
+            ((gameBoardArray[0] == player && gameBoardArray[3] == player && gameBoardArray[6] == player) ||
+             (gameBoardArray[1] == player && gameBoardArray[4] == player && gameBoardArray[7] == player) || 
+             (gameBoardArray[2] == player && gameBoardArray[5] == player && gameBoardArray[8] == player)) {
+                console.log("Winner");   
+                return true;
+            }
+        //return false if no wins found
+        return false;
+    }
+
+    //function add the current player's counter to their chosen position
+    const addCounterToBoard = (square) => {
+        //add to the correct position in the board array
+        //then redraw the board
+        //console.log(square);
+        
+        //get the id of the selected square by
+        //slicing the last character of the id attribute
+        //check that the selected element is blank
+        if (gameBoardArray[square.srcElement.id.slice(-1)] == " ") {
+            gameBoardArray[square.srcElement.id.slice(-1)] = currentPlayer
+            //add visual indicator
+            square.srcElement.classList.add("unselectable");
+            //redraw the board
+            displayController.renderBoard(getBoardStatus());
+            //check for winner
+            if (checkforWinner(currentPlayer)) {
+                alert(`Player ${currentPlayer} is the Winner!`)
+            }
+            switchPlayer();
+        } else {
+
+            console.log(square);
+        }
+
     }
 
     //return the current board game status array
@@ -38,7 +111,7 @@ const gameBoard = (() => {
 	};
 
     //make functions available outside of this module
-    return{newGame, getBoardStatus};
+    return{newGame, getBoardStatus, addCounterToBoard};
 
 })();
 
@@ -57,7 +130,7 @@ const displayController = (() => {
     
     //add listener events for squares on the board
     boardGameDOM.forEach((square) => {
-        square.addEventListener("click", test);
+        square.addEventListener("click", gameBoard.addCounterToBoard);
     });
 
     //copy the contents of gameAray object passed in as a parameter onto the board DOM
@@ -81,9 +154,7 @@ const displayController = (() => {
 
 //initial function to execute on page load
 const enterPage = function () {
-    //gameBoard.initialiseGameBoard();
-    //displayController.renderBoard(gameBoard.getBoardStatus());
-    //displayController.addBoardListeners();
+    gameBoard.newGame();
 }
 
 
